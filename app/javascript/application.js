baf8288c42fc657eb62fc9185f531d629485ea3d
Jonathan Marin Salazar <jonathanmarinsalazar@gmail.com>
2024-05-18 15:31:51 -0500

    fixes bug with flash messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,29 +3,33 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 
-var minus = document.querySelector(".btn-subtract")
-var add = document.querySelector(".btn-add");
-var quantityNumber = document.querySelector(".item-quantity");
-var currentValue = 1;
+document.addEventListener("turbo:load", (event) => {
+  console.log("turbo:load. runs every time a page is loaded");
+  var minus = document.querySelector(".btn-subtract")
+  var add = document.querySelector(".btn-add");
+  var quantityNumber = document.querySelector(".item-quantity");
+  var currentValue = 1;
 
-if(minus){
-  minus.addEventListener("click", function(){
-    if(quantityNumber.value === '') { quantityNumber.value = 1; }
+  if(minus){
+    minus.addEventListener("click", function(){
+      if(quantityNumber.value === '') { quantityNumber.value = 1; }
 
-    currentValue = parseInt(quantityNumber.value);
-    if (currentValue > 1) {
-      currentValue -= 1;
+      currentValue = parseInt(quantityNumber.value);
+      if (currentValue > 1) {
+        currentValue -= 1;
+        quantityNumber.value = currentValue;
+      }
+    });
+  }
+
+  if(add){
+    add.addEventListener("click", function() {
+      if(quantityNumber.value === '') { quantityNumber.value = 0; }
+
+      currentValue = parseInt(quantityNumber.value);
+      currentValue += 1;
       quantityNumber.value = currentValue;
-    }
-  });
-}
+    });
+  }
+});
 
-if(add){
-  add.addEventListener("click", function() {
-    if(quantityNumber.value === '') { quantityNumber.value = 0; }
-
-    currentValue = parseInt(quantityNumber.value);
-    currentValue += 1;
-    quantityNumber.value = currentValue;
-  });
-}
